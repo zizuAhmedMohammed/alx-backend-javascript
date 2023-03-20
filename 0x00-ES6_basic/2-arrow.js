4d0f5e33bb1648b2bbe31450963fd7ce52bc3061
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-03-20 16:21:38 +0000

    Modfiy Task 2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,6 +2,7 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
+
   this.addNeighborhood = (newNeighborhood) => {
 	self.sanFranciscoNeighborhoods.push(newNeighborhood);
 
