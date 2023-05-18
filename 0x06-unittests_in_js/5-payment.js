b6349bbb54cfb0d1d83085527b2a744c5e007a8d
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-05-18 19:16:49 +0000

    Task 5

---
+++ 0x06-unittests_in_js/5-payment.js
@@ -0,0 +1,7 @@
+Utils = require('./utils');
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const output = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${output}`);
+}
+
+module.exports = sendPaymentRequestToApi;
